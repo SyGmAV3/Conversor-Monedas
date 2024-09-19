9e3b87e6f09c1576a1d5ad84e9a147677abb2345
Andrés L. Ospina Gutiérrez <dogpi@hotmail.com>
2024-09-19 15:11:02 -0500

    Conversor Monedas

---
+++ src/com/sygma/app/IniciarApp.java
@@ -0,0 +1,8 @@
+package com.sygma.app;
+public class IniciarApp {
+    public static void main(String[] args) {
+        //Crear una instancia de MenuPrincipal y llamar al metodo iniciar
+        MenuPrincipal inicio = new MenuPrincipal();
+        inicio.iniciar();
+    }
+}
